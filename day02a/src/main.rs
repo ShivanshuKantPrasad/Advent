c10c51284f9c4f35ab25f1f8dfab21f7f359fd4a
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-14 13:00:34 +0530

    Add day02a

---
+++ day02a/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let rounds = input.lines();
+    let mut total_score = 0;
+    for round in rounds {
+        let a: Vec<&str> = round.split(" ").collect::<_>();
+        let mut score = match a[1] {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => 0,
+        };
+        score += match (a[0], a[1]) {
+            ("A", x) => match x {
+                "X" => 3,
+                "Y" => 6,
+                "Z" => 0,
+                _ => 0,
+            },
+            ("B", x) => match x {
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => 0,
+            },
+            ("C", x) => match x {
+                "X" => 6,
+                "Y" => 0,
+                "Z" => 3,
+                _ => 0,
+            },
+            _ => 0,
+        };
+        total_score += score;
+    }
+    println!("Answer: {:?}", total_score);
+}
