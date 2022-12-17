972af957d91b003b37615e65a4180ed2a9002c6d
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-17 20:20:39 +0530

    Add day06b

---
+++ day06b/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .windows(14)
+            .position(|b| !(0..13).any(|i| (i + 1..14).any(|j| b[i] == b[j])))
+            .unwrap()
+            + 14,
+    );
+}
