3b36e4ab055076367a1f56a091ef39f8e006a6ab
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-17 08:57:32 +0530

    Add day05a

---
+++ day05a/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let data = include_str!("../input.txt");
+    let (crates, instructions) = data.split_once("\n\n").unwrap();
+    let length = crates
+        .lines()
+        .last()
+        .unwrap()
+        .split(" ")
+        .last()
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    println!("{length}");
+    let mut stacks: [Vec<char>; 10] = Default::default();
+    for line in crates.lines().rev().skip(1) {
+        let mut i = 0;
+        for char in line.chars() {
+            i += 1;
+            if char.is_alphabetic() {
+                // 2 -> 0, 6 -> 1, 10 -> 2
+                // (n - 2) / 4
+                stacks[(i - 2) / 4].push(char);
+            }
+        }
+    }
+    for instruction in instructions.lines() {
+        let bit = instruction
+            .split(" ")
+            .filter_map(|x| x.parse::<usize>().ok())
+            .collect::<Vec<_>>();
+        if let [number, from, to] = bit[..] {
+            for _ in 0..number {
+                let temp = stacks[from - 1].pop().unwrap();
+                stacks[to - 1].push(temp);
+            }
+        }
+    }
+
+    for stack in stacks {
+        println!("{stack:?}");
+    }
+}
